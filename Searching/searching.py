5a406f95f177c26fc01aba667a4ea640425c7a79
Admin <admin@Apples-MacBook-Pro-6.local>
2020-02-16 17:50:59 -0500

    Added Binary Search

--- Searching/searching.py
+++ Searching/searching.py
@@ -1,6 +1,6 @@
 ''' Linear Search O(n)
     Avg/Worst Case Performace: O(n)
-    Best Case Performance (first element in array): O(1)
+    Best Case Performance (1st element in array): O(1)
 '''
 def linear_search(n,arry):
     for i in range(len(arry)):
@@ -8,5 +8,31 @@ def linear_search(n,arry):
             return i 
     return -1 
 
-def binary_search(n,arry):
-    pass
+''' Binary Search O(log n)
+    Takes pre-sorted array as input
+    Avg/Worst Case performance: O(log n)
+    Best Case Performance (1st elem in array): O(1)
+'''
+def binary_search_iterative(n,arry):
+    low = 0
+    high = len(arry) - 1
+
+    while low <= high:
+        mid = (low + high) // 2 
+        if n == arry[mid]: 
+            return mid
+        elif n < arry[mid]: 
+            high = mid - 1
+        else: 
+            low = mid + 1
+    return -1
+
+def binary_search_recursive(n,arry,low,high):
+    if low > high: return -1 #Base case
+    mid =(low + high) // 2
+    if n == arry[mid]: 
+        return mid
+    elif n < arry[mid]: 
+        return binary_search_recursive(n,arry,low,mid-1)
+    else: 
+        return binary_search_recursive(n,arry,mid+1,high)
--- Searching/searching_test.py
+++ Searching/searching_test.py
@@ -1,5 +1,5 @@
 import unittest
-from searching import linear_search, binary_search
+from searching import linear_search, binary_search_iterative,binary_search_recursive
 
 class TestSearching(unittest.TestCase):
     def test_linear_search(self):
@@ -7,7 +7,8 @@ class TestSearching(unittest.TestCase):
         self.assertEqual(linear_search(14,arry),3)
     def test_binary_search(self):
         arry = [2,3,4,10,40]
-        self.assertEqual(binary_search(10,arry),4)
+        self.assertEqual(binary_search_iterative(10,arry),3)
+        self.assertEqual(binary_search_recursive(10,arry,0,len(arry)-1),3)
 
 if __name__ == '__main__':
     unittest.main()
